73bb8df9d6db55de9e12c689cbc3d5a785a261f2
duohy01 <94445890+duohy01@users.noreply.github.com>
2022-01-05 01:54:41 +0700

    hii

--- head.php
+++ head.php
@@ -16,4 +16,11 @@
 
 <body>
 
-    
+    <?php
+    foreach ($_POST as $variable => $value) {
+        $_POST["$variable"] = addslashes(htmlspecialchars($value));
+    }
+    foreach ($_GET as $variable => $value) {
+        $_GET["$variable"] = addslashes(htmlspecialchars($value));
+    }
+    ?>
