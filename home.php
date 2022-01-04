a5ae084059c2ebccd38a8f8b3aa73039cf475d28
duohy01 <94445890+duohy01@users.noreply.github.com>
2022-01-05 02:02:34 +0700

    Update home.php
    hiii

--- home.php
+++ home.php
@@ -7,4 +7,47 @@ if (!isset($_SESSION['isLoginOK'])) {
 }
 ?>
 
-    
+    <header class="container-fluid">
+        <div class="navbar fixed-top navbar-expand-lg navbar-light bg-light d-flex align-items-center d-flex justify-content-center">
+            <div class="container-fluid" style="margin-left: 30%;">
+                <a class="navbar-brand " href="home.php"><img src="documents/foody.png" class="img-fluid"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                    <ul class="navbar-nav  ">
+
+                        <li class="nav-item dropdown mt-sm-1" style="font-size: 18px;">
+                            <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Vị trí</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Hà Nội</a></li>
+                                <li><a class="dropdown-item" href="#">TP. Hò Chí Minh</a></li>
+                                <li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown  mt-sm-1 " style="font-size: 18px;">
+                            <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Loại hình</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Đồ ăn</a></li>
+                                <li><a class="dropdown-item" href="#">Đồ uống</a></li>
+                            </ul>
+                        </li>
+
+                        <form class="d-flex">
+                            <div class="input-group ">
+                                <input type="text" class="form-control mt-sm-2" aria-label="Recipient's username" aria-describedby="basic-addon2" style="height: 30px;">
+                                <div class="input-group-append">
+                                    <span class="input-group-text mt-md-2" id="basic-addon2"><i class="fas fa-search"></i></span>
+                                </div>
+                            </div>
+                        </form>
+
+                        <ul class="navbar-nav  ">
+                            <li class="nav-item dropdown  mt-sm-1 " style="font-size: 18px;">
+                                <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+
+                                   
+
+                                    
