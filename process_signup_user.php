8b269b1490fa40d62bc1a5aa0704528294c8f23a
duohy01 <94445890+duohy01@users.noreply.github.com>
2022-01-06 09:35:51 +0700

    hi4

--- process_signup_user.php
+++ process_signup_user.php
@@ -18,4 +18,27 @@
     if(!$conn){
         die("Kết nối thất bại. Vui lòng kiểm tra lại các thông tin máy chủ");
     }
+    // Bước 02: Thực hiện truy vấn
+    $sql01 = "SELECT * FROM db_user WHERE user_email = '$email' OR user_name = '$user'";
+    // Ở đây còn có các vấn đề về tính hợp lệ dữ liệu nhập vào FORM
+    // Nghiêm trọng: lỗi SQL Injection
+
+    $result01 = mysqli_query($conn,$sql01);
+    if(mysqli_num_rows($result01) > 0){
+        $error = "Tài khoản đã tồn tại!";
+        header("location: signup_user.php?error=$error"); //Chuyển hướng, hiển thị thông báo lỗi
+    }else{
+        $pass_hash = password_hash($pass1, PASSWORD_DEFAULT);
+        $sql02 = "INSERT INTO db_user (user_name, user_email, user_pass) VALUES('$user', '$email', '$pass_hash')";
+        $result02 = mysqli_query($conn,$sql02);
+        if($result02 == true){
+            header("location: login.php");
+        }else{
+            $error = "Can not insert record. Please check ....";
+            header("location: signup_user.php?error=$error");
+    }
     
+}
+// Bước 03: Đóng kết nối
+mysqli_close($conn);
+?>
