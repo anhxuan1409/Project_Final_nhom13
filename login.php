1217d2c0fdeb264c612ba7c0dcea21f59e07499e
duohy01 <94445890+duohy01@users.noreply.github.com>
2022-01-09 16:46:33 +0700

    hi333

--- login.php
+++ login.php
@@ -26,4 +26,29 @@ require "template/header.php";
                             </span>
                             <input type="email" id="inputEmail" name="txtEmail" class="form-control" placeholder="Email address" required autofocus>
 
+                            </div>
+                        <div class="form-space input-group">
+                            <span class="input-group-addon">
+                                <span class="glyphicon glyphicon-lock"></span>
+                            </span>
+                            <input type="password" id="inputPassword" name="txtPass" class="form-control" placeholder="Password" required>
+                        </div>
+                        <div class="checkbox" style="margin:10px 0;">
+                            <label style="float:left;">
+                                <input type="checkbox"> Lưu mật khẩu
+                            </label>
+                        </div>
+
+                        <div style="clear:both;float:left;width:100%;margin-top:15px;" class="d-flex align-items-center mb-2 d-flex justify-content-center text-center">
+                            <button class="btn btn-lg btn-block mt-3 d-flex align-items-center mb-2 d-flex justify-content-center text-center" type="submit" name="btnSignIn" style="background-color:#0495ba;font-size: 14px;border-color:#0495ba">Đăng nhập</button>
+                        </div>
+                        <div class="mt-3 mb-3 justify-content-center justify-content-center text-center">
+                        <?php
+                            if (isset($_GET['error'])) {
+                                echo "<div style='color:red; margin-left: 30px'> {$_GET['error']} </div>";
+                            }
+
+                            ?>
+                        </div>
+
                         
