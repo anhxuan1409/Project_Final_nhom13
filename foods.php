956147279601b1840b57c18a9c3ec14d1c2f7abc
Chính <chinhtogaming05@gmail.com>
2022-01-12 10:10:13 +0700

    up

--- foods.php
+++ foods.php
@@ -9,3 +9,29 @@ if (!$result) {
 }
 
 ?>
+<div class="container-fluid">
+    <div class="row">
+        <?php while ($query_row = mysqli_fetch_assoc($result)) { ?>
+            <div class="col-md-2 ms-3 me-2 mb-5 text-center zoom zoom">
+                <a href="food.php?foodisbn=<?php echo $query_row['food_isbn']; ?>" class="text-decoration-none text-dark">
+                    <div class="card" style="width: 15rem; height:25rem">
+                        <img src="<?php echo $query_row['food_image']; ?>" class="card-img-top" style="height: 75%;">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $query_row['food_title'] ?></h5>
+                        </div>
+                    </div>
+                </a>
+
+
+            </div>
+        <?php } ?>
+        <style>
+               img {
+                height: 70px;
+                width: 120px;
+                object-fit: cover;
+                padding-bottom: 15px ;}
+                </style>
+    </div>
+
+</div>
