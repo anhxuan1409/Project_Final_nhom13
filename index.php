fad9dc6b352d24a4940c59107ae545c2a5ba5ae6
Chính <chinhtogaming05@gmail.com>
2022-01-12 09:47:37 +0700

    up

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+include('header.php')
+?>
+
+<div class="slide container-fluid">
+        <div class="col-lg-12">
+            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner ">
+                    <div class="carousel-item active" data-bs-interval="10000">
+                        <img src="img/maxresdefault.jpg" class="d-block img-fluid rounded mx-auto d-block" style="width: 65%;">
+                        <div class="carousel-caption d-none d-md-block">
+
+                        </div>
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src="img/occhao.jpg" class="d-block img-fluid rounded mx-auto d-block" style="width: 65%;">
+                        <div class="carousel-caption d-none d-md-block">
+
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/photo3jpg_822.jpg" class="d-block img-fluid rounded mx-auto d-block" style="width: 65%;">
+                        <div class="carousel-caption d-none d-md-block">
+
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+
+     </div>
+
+
+
+    
